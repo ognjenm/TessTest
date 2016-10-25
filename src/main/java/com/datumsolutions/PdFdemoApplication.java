7fcd48880ea8e3eff3a17f87c27fde1af0b1656b
Ognjen Miletic <ognjen.miletic@gmail.com>
2016-10-25 10:25:21 +0200

    refactoring

--- src/main/java/com/datumsolutions/PdFdemoApplication.java
+++ src/main/java/com/datumsolutions/PdFdemoApplication.java
@@ -19,8 +19,8 @@ public class PdFdemoApplication {
 
 	public static void main(String[] args) throws IOException {
 		Properties properties = new Properties();
-		String inputPdf = args[0];
-		String outputPdf = args[1];
+		String inputPdf = "/Users/ognjenm/code/open_source/testPdf/files/scan.pdf";
+		String outputPdf = "./out"; // no extension needed
 
 		try {
 
@@ -40,112 +40,14 @@ public class PdFdemoApplication {
 				e.printStackTrace();
 			}
 
-			String tempdirProperty = "java.io.tmpdir";
-			String tempDirPath = System.getProperty(tempdirProperty);
-			System.out.println("OS current temporary directory is " + tempDirPath);
-			File tempDir = FileUtils.createTempDir();
-			//USING WORKING DIR FOR DEBUG
-			//COMMENT THIS IN PRODUCTION
-			// Below is screnshot of settings in Intelij
-			// https://www.dropbox.com/s/1ttb8lwn2qfbz0m/Screenshot%202016-10-24%2017.02.34.png?dl=1
-			//tempDir = new File("/Users/ognjenm/code/open_source/testPdf/WORKINGDIR/png");
-
-			TesseractCustom tessaractInstance = new TesseractCustom();
-			tessaractInstance.setLanguage(properties.getProperty("lang","eng"));
-			tessaractInstance.setDatapath(properties.getProperty("tessdata.path","#"));
-			// check for tessdata dir path from configuration
-			// else use bundled tessdata
-			if("#".equals(properties.getProperty("tessdata.path")))
-			{
-				tessaractInstance.setDatapath(properties.getProperty("tessdata.path"));
-			}
-
-
-			//makePdfWithUpscaledImages(tessaractInstance, inputPdf, outputPdf);
-
-			makePdfWithOriginalImages(tessaractInstance, inputPdf, outputPdf, tempDir);
-
+			ProcessPdfDocument processPdfDocument = new ProcessPdfDocument(properties);
+			processPdfDocument.doOCR(inputPdf, outputPdf, true);
 
 		} catch (TesseractException e) {
 			e.printStackTrace();
 		}
 	}
 
-
-	private static void makePdfWithOriginalImages(TesseractCustom tessaractInstance, String inputPdf, String outputPdf, File tempDir) throws TesseractException {
-		//HOCR
-		tessaractInstance.setHocr(true);
-		File inputFile = new File(inputPdf);
-
-		//Export Images from PDF into pngs 300ppi resolution
-		File[] files = PdfUtilities.convertPdf2Png(inputFile,tempDir);
-
-		List<String> orig = null;
-		try {
-			orig = PdfUtilities.getImagesFromPdf(inputPdf, tempDir);
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		if(orig.size()!=files.length)
-		{
-			//TODO: Falback to makePdfWithUpscaledImages ???
-			throw new TesseractException("Different number of pages detected");
-		}
-
-		int counter = 0;
-		for (File file : files) {
-			String hocrResult = tessaractInstance.doOCR(file);
-			//replace working images with original
-
-			// I think that I don't need this????
-			hocrResult = hocrResult.replace(file.getAbsolutePath(), orig.get(counter));
-
-			try {
-				FileOutputStream os;
-				String pdfFile = file.getAbsolutePath().replaceFirst("[.][^.]+$", "") +".pdf";
-				os = new FileOutputStream(pdfFile);
-				InputStream stream = new ByteArrayInputStream(hocrResult.getBytes("UTF-8"));
-
-				HocrToPdf hocrToPdf = new HocrToPdf(os);
-				hocrToPdf.addHocrDocument(stream, new FileInputStream(new File(orig.get(counter))));
-				hocrToPdf.setPdfFormat(PDFF.PDF_A_1B);
-				hocrToPdf.convert();
-				os.close();
-				stream.close();
-				counter++;
-
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
-		}
-
-		// find resulting pdf files and order them
-		File[] workingFiles = tempDir.listFiles(new FilenameFilter() {
-			@Override
-			public boolean accept(File dir, String name) {
-				return name.toLowerCase().matches("workingimage\\d{3}\\.pdf$");
-			}
-		});
-
-		Arrays.sort(workingFiles, new Comparator<File>() {
-			@Override
-			public int compare(File f1, File f2) {
-				return f1.getName().compareTo(f2.getName());
-			}
-		});
-
-
-		PdfUtilities.mergePdf(workingFiles, new File(outputPdf + ".pdf"));
-		//FileUtils.deleteDirectory(tempDir);
-	}
-
-	private static void makePdfWithUpscaledImages(TesseractCustom tessaractInstance, String inputPdf, String outputPdf) throws TesseractException {
-		// DIRECTLY GENERATE PDF
-		List<ITesseract.RenderedFormat> list = new ArrayList<ITesseract.RenderedFormat>();
-		list.add(ITesseract.RenderedFormat.PDF);
-		tessaractInstance.createDocuments(inputPdf, outputPdf, list);
-
-	}
 
 
 }
---
+++ src/main/java/com/datumsolutions/ProcessPdfDocument.java
@@ -0,0 +1,125 @@
+package com.datumsolutions;
+
+import com.datumsolutions.util.FileUtils;
+import com.datumsolutions.util.PdfUtilities;
+import com.googlecode.jhocr.converter.HocrToPdf;
+import com.googlecode.jhocr.util.enums.PDFF;
+import net.sourceforge.tess4j.ITesseract;
+import net.sourceforge.tess4j.TesseractException;
+
+import java.io.*;
+import java.util.*;
+
+/**
+ * Created by ognjenm on 25/10/16.
+ */
+public class ProcessPdfDocument {
+
+    private TesseractCustom tessaractInstance;
+
+
+    public ProcessPdfDocument(Properties properties)
+    {
+        tessaractInstance = new TesseractCustom();
+        tessaractInstance.setLanguage(properties.getProperty("lang","eng"));
+        tessaractInstance.setDatapath(properties.getProperty("tessdata.path","#"));
+        // check for tessdata dir path from configuration
+        // else use bundled tessdata
+        if("#".equals(properties.getProperty("tessdata.path")))
+        {
+            tessaractInstance.setDatapath(properties.getProperty("tessdata.path"));
+        }
+    }
+
+    public void doOCR(String inputPdf, String outputPdf, Boolean returnOriginalImages) throws TesseractException {
+
+        if(returnOriginalImages)
+        {
+            this.makePdfWithOriginalImages(inputPdf, outputPdf);
+        }
+        else {
+            this.makePdfWithUpscaledImages(inputPdf, outputPdf);
+        }
+    }
+
+
+    private void makePdfWithOriginalImages(String inputPdf, String outputPdf) throws TesseractException {
+
+        String tempdirProperty = "java.io.tmpdir";
+        String tempDirPath = System.getProperty(tempdirProperty);
+        System.out.println("OS current temporary directory is " + tempDirPath);
+        File tempDir = FileUtils.createTempDir();
+
+        //HOCR
+        tessaractInstance.setHocr(true);
+        File inputFile = new File(inputPdf);
+
+        //Export Images from PDF into pngs 300ppi resolution
+        File[] files = PdfUtilities.convertPdf2Png(inputFile,tempDir);
+
+        List<String> orig = null;
+        try {
+            orig = PdfUtilities.getImagesFromPdf(inputPdf, tempDir);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        if(orig.size()!=files.length)
+        {
+            //TODO: Falback to makePdfWithUpscaledImages ???
+            throw new TesseractException("Different number of pages detected");
+        }
+
+        int counter = 0;
+        for (File file : files) {
+            String hocrResult = tessaractInstance.doOCR(file);
+            //replace working images with original
+
+            // I think that I don't need this????
+            hocrResult = hocrResult.replace(file.getAbsolutePath(), orig.get(counter));
+
+            try {
+                FileOutputStream os;
+                String pdfFile = file.getAbsolutePath().replaceFirst("[.][^.]+$", "") +".pdf";
+                os = new FileOutputStream(pdfFile);
+                InputStream stream = new ByteArrayInputStream(hocrResult.getBytes("UTF-8"));
+
+                HocrToPdf hocrToPdf = new HocrToPdf(os);
+                hocrToPdf.addHocrDocument(stream, new FileInputStream(new File(orig.get(counter))));
+                hocrToPdf.setPdfFormat(PDFF.PDF_A_1B);
+                hocrToPdf.convert();
+                os.close();
+                stream.close();
+                counter++;
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        // find resulting pdf files and order them
+        File[] workingFiles = tempDir.listFiles(new FilenameFilter() {
+            @Override
+            public boolean accept(File dir, String name) {
+                return name.toLowerCase().matches("workingimage\\d{3}\\.pdf$");
+            }
+        });
+
+        Arrays.sort(workingFiles, new Comparator<File>() {
+            @Override
+            public int compare(File f1, File f2) {
+                return f1.getName().compareTo(f2.getName());
+            }
+        });
+
+        PdfUtilities.mergePdf(workingFiles, new File(outputPdf + ".pdf"));
+        FileUtils.deleteDirectory(tempDir);
+    }
+
+    private void makePdfWithUpscaledImages(String inputPdf, String outputPdf) throws TesseractException {
+        // DIRECTLY GENERATE PDF
+        List<ITesseract.RenderedFormat> list = new ArrayList<ITesseract.RenderedFormat>();
+        list.add(ITesseract.RenderedFormat.PDF);
+        tessaractInstance.createDocuments(inputPdf, outputPdf, list);
+    }
+
+}
